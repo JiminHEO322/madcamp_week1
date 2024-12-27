4cb833871a42eb3967006d614c4dcc236cb6bf6a
Jimin Heo <zimin322@gmail.com>
2024-12-27 22:29:15 +0900

    event detail page layout

--- app/src/main/java/com/example/exhibition/ui/event/EventDetailActivity.kt
+++ app/src/main/java/com/example/exhibition/ui/event/EventDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.exhibition.ui.event
 
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,11 @@ class EventDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_detail)
 
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            title = "상세 정보"
+        }
+
         val imageResId = intent.getIntExtra("EVENT_IMAGE", R.drawable.cat1)
         val title = intent.getStringExtra("EVENT_TITLE")
         val location = intent.getStringExtra("EVENT_LOCATION")
@@ -24,18 +30,10 @@ class EventDetailActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.detailEventTitle).text = title
         findViewById<TextView>(R.id.detailEventLocation).text = location
         findViewById<TextView>(R.id.detailEventDate).text = date
+    }
 
-//        val title = intent.getStringExtra("PLACE_TITLE")
-//        val addressAll = intent.getStringExtra("PLACE_ADDRESS")
-//        phone = intent.getStringExtra("PLACE_PHONE")
-//        val exNum = intent.getStringExtra("PLACE_EXNUM")
-//        val imageResId = intent.getIntExtra("PLACE_IMAGE", R.drawable.sample_image)
-//
-//
-//        findViewById<TextView>(R.id.detailTitleTextView).text = title
-//        findViewById<TextView>(R.id.detailAddressTextView).text = addressAll
-//        //findViewById<TextView>(R.id.detailPhoneTextView).text = phone
-//        //findViewById<TextView>(R.id.detailExNumTextView).text = exNum
-//        findViewById<ImageView>(R.id.detailImageView).setImageResource(imageResId)
+    override fun onSupportNavigateUp(): Boolean {
+        finish() // 현재 Activity 종료
+        return true
     }
 }
